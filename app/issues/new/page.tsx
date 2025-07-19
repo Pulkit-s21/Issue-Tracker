5c5b89edc33018cd27101271b1a4963481607634
Pulkit-s21 <pulkitsaxena795@gmail.com>
2025-07-19 13:47:27 +0530

    Issue submission form & sending data to db thru api

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,13 +1,40 @@
-import { Button, TextField } from "@radix-ui/themes"
+"use client"
+
 import MarkdownEditor from "@/components/MarkdownEditor"
+import axios from "axios"
+import { Button, TextField } from "@radix-ui/themes"
+import { useForm, Controller, SubmitHandler } from "react-hook-form"
+import { useRouter } from "next/navigation"
+
+interface IssueForm {
+  title: string
+  description: string
+}
 
 const NewIssuePage = () => {
+  const { register, control, handleSubmit } = useForm<IssueForm>()
+
+  const router = useRouter()
+  const onSubmit: SubmitHandler<IssueForm> = async (data) => {
+    await axios.post("/api/issues", data)
+    router.push("/issues")
+  }
+
   return (
-    <div className="flex flex-col gap-4 max-w-xl">
-      <TextField.Root placeholder="Issue title" />
-      <MarkdownEditor />
+    <form
+      className="flex flex-col gap-4 max-w-xl"
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <TextField.Root placeholder="Issue title" {...register("title")} />
+      <Controller
+        name="description"
+        control={control}
+        render={({ field }) => (
+          <MarkdownEditor placeholder="Description" {...field} />
+        )}
+      />
       <Button>Submit issue</Button>
-    </div>
+    </form>
   )
 }
 
