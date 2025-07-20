5225423afcc4295c63b8045edaf521504fbdfd65
Pulkit-s21 <pulkitsaxena795@gmail.com>
2025-07-20 11:45:36 +0530

    Implemented client-side validations & refactoring

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -2,18 +2,25 @@
 
 import MarkdownEditor from "@/components/MarkdownEditor"
 import axios from "axios"
-import { Button, Callout, TextField } from "@radix-ui/themes"
+import { Button, Callout, Text, TextField } from "@radix-ui/themes"
 import { useForm, Controller, SubmitHandler } from "react-hook-form"
 import { useRouter } from "next/navigation"
 import { useState } from "react"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { z } from "zod"
+import { createIssueSchema } from "@/app/validationSchemas"
 
-interface IssueForm {
-  title: string
-  description: string
-}
+type IssueForm = z.infer<typeof createIssueSchema> // only need to change form validations in 1 place only now
 
 const NewIssuePage = () => {
-  const { register, control, handleSubmit } = useForm<IssueForm>()
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema),
+  })
   const [error, setError] = useState("")
 
   const router = useRouter()
@@ -41,6 +48,11 @@ const NewIssuePage = () => {
         onSubmit={handleSubmit(onSubmit)}
       >
         <TextField.Root placeholder="Issue title" {...register("title")} />
+        {errors.title && (
+          <Text color="red" as="p">
+            {errors.title.message}
+          </Text>
+        )}
         <Controller
           name="description"
           control={control}
@@ -48,6 +60,11 @@ const NewIssuePage = () => {
             <MarkdownEditor placeholder="Description" {...field} />
           )}
         />
+        {errors.description && (
+          <Text color="red" as="p">
+            {errors.description.message}
+          </Text>
+        )}
         <Button>Submit issue</Button>
       </form>
     </div>
